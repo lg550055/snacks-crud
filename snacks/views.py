2a384dff202ccfef10a0cea635184c33d22c847a
polo <lg550055@hotmail.com>
2022-05-25 19:01:27 -0500

    First commit

---
+++ snacks/views.py
@@ -0,0 +1,31 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Snack
+
+
+class SnackListView(ListView):
+  template_name = 'snack_list.html'
+  model = Snack
+
+
+class SnackDetailView(DetailView):
+  template_name = 'snack_detail.html'
+  model = Snack
+
+
+class SnackUpdateView(UpdateView):
+    template_name = "snack_update.html"
+    model = Snack
+    fields = ["name", "description", "purchaser"]
+
+
+class SnackCreateView(CreateView):
+    template_name = "snack_create.html"
+    model = Snack
+    fields = ["name", "description", "purchaser"]
+
+
+class SnackDeleteView(DeleteView):
+    template_name = "snack_delete.html"
+    model = Snack
+    success_url = reverse_lazy("snack_list")
